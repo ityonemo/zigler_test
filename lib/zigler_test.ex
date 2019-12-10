334d7f5154d1843d8e273511654008e3da640167
ityonemo <isaac.yonemoto@gmail.com>
2019-12-09 19:09:16 -0800

    first commit

---
+++ lib/zigler_test.ex
@@ -0,0 +1,18 @@
+defmodule ZiglerTest do
+  @moduledoc """
+  Documentation for ZiglerTest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZiglerTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zigler_test_test.exs
@@ -0,0 +1,8 @@
+defmodule ZiglerTestTest do
+  use ExUnit.Case
+  doctest ZiglerTest
+
+  test "greets the world" do
+    assert ZiglerTest.hello() == :world
+  end
+end
